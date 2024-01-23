109c5f195ea6ebd8d5aa268d093a104a35432eec
Collins <ochiengcollinsoduor2@gmail.com>
2024-01-24 01:02:37 +0300

    backup

--- 0x14-mysql/tyrell_corp.sql
+++ 0x14-mysql/tyrell_corp.sql
@@ -26,7 +26,7 @@ CREATE TABLE `nexus6` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
