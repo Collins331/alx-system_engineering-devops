a6d7d287f9ee601c6ac6978dbe64a4fde44db2ec
Collins Ochieng <125954874+Collins331@users.noreply.github.com>
2024-01-29 23:01:51 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,35 +1,26 @@
 #!/usr/bin/python3
-"""import requests module"""
+"""Gather data from an API"""
 
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    """
-    prevents script execution when imported
-    as a module in other programs
-    """
-    completed = 0
-    total = 0
-    title = []
-    url = f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}"
-    r = requests.get()
-    r = r.json()
-    name = r.get("name")
-    url = f"https://jsonplaceholder.typicode.com/todos?userId={sys.argv[1]}"
-    t = requests.get(url)
-    t = t.json()
-    for i in t:
-        if i.get("completed"):
-            completed += 1
-            total += 1
-            title.append(i.get("title"))
-        else:
-            total += 1
-    # print(t)
+    # Define the URL for the REST API
+    url = "https://jsonplaceholder.typicode.com/"
 
-    print("Employee {} is done with tasks({}/{}):"
-          .format(name, completed, total))
-    for tc in title:
-        print("\t {}".format(tc))
+    # send a GET request to retrieve user info
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+
+    # send a GET request to retrive the TODO list
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    # filter completed TODO list and store titles in a list
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+
+    # print employee's name, completed tasks & total no of tasks
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+
+    # print the titles of completed tasks with indentation
+    [print("\t {}".format(c)) for c in completed]
